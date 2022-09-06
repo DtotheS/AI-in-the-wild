ff95153a24cc82ac273dea5ad2585d19617db288
Data to the Science <to.sian.lee@gmail.com>
2022-09-05 23:51:47 -0400

    Update aap_crawler.py
    1. updated ratings - completed
    2. period updated: 08/31/2022 ~ very beginning.

--- scr/aap_crawler.py
+++ scr/aap_crawler.py
@@ -52,7 +52,6 @@ for i in range(43):
 
 # id,title,url,claim,rating,content_owner,author_name,date_published,date_updated,primary_category,tags,sourceid,sources_num,sources,bodyt,page_type,yearp,monthp,dayp
 df = pd.DataFrame()
-df['id'] = range(1,len(links)+1)
 df['link'] = links
 df['site'] = "AAP"
 
@@ -102,14 +101,205 @@ df['fc_date'] = fc_dates
 
 df.to_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_083022.csv", index=False)
 
+##########################Insert updated links at the top#################################################
+'''
+ Condition 1: Do not know the total number of links before finishing to collect data.
+    C2: Need to assign largest negative number to the latest link
+    C3: Crawling starts from the latest link.
+    C4: The oldest link should be indexed as -1. 
+ 
+ Solution:
+    First, assign the latest: 0 1 2 3 4 5 6 .... : the oldest (let's say total 7 links)
+    0-7 = -7
+    1-7 = -6
+    ...
+    6-7 = -1
+'''
 
-######################## Select only 2019 ~ 2021 years' data #######################
+df = pd.read_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_083022.csv")
+del df['id']
+df.columns
+
+links = ["https://www.aap.com.au/factcheck/brain-damage-link-to-msg-a-salty-dose-of-misinformation/","https://www.aap.com.au/factcheck/microwave-meme-cooks-up-cancer-food-fears/"] # Same older with existing crawlers: latest to oldest
+
+# Can use excatly same crwaling method
+df2 = pd.DataFrame()
+df2['link'] = links
+df2['site'] = "AAP"
+
+titles = []
+authors = []
+claims = []
+ratings = []
+fc_dates = []
+for i in range(len(df2)):
+    driver.get(df2['link'][i])
+    titles.append(driver.find_element_by_tag_name("h1").text)  # title
+    authors.append(driver.find_element_by_class_name("info").find_elements_by_tag_name("span")[0].text)  # author name
+    fc_dates.append(driver.find_element_by_class_name("info").find_elements_by_tag_name("span")[1].text)  # FC date
+    try:
+        claims.append(driver.find_elements_by_class_name("inner")[0].text.strip().split("\n")[1])  # claim
+    except:
+        claims.append(None)     # page15 ~ : no claim, but only statement.
+    try:
+        ratings.append(driver.find_element_by_class_name("c-article__verdict").find_element_by_tag_name("strong").text)  # rating
+    except:
+        ratings.append(None)
+    # driver.find_elements_by_class_name("inner")[1].text.strip().split("\n")[1].strip().split(". ")[1] # Rating Reason summary
+    print(str(i) +" "+df2['link'][i] +" done")
+
+df2['title'] = titles
+df2['claim'] = claims
+df2['rating'] = ratings
+df2['author'] = authors
+df2['fc_date'] = fc_dates
+
+ii = len(df2)
+for i in range(len(df2)):
+    df.loc[i-ii] = df2.loc[i]
+
+df.index = df.index + len(df2)
+df = df.sort_index()
+
+df.to_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_090122.csv", index=False)
+
+################ Rating Update ###################
+
+df = pd.read_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_090122.csv")
+rcan = list(set(df['rating']))
+for i in range(len(rcan)):
+    print(str(i) + " " + rcan[i])
+len(rcan[27]) # 19: max correct label len.
+
+# driver.get(df[df['rating']==rcan[5]]['link'].iloc[0])
+import json
+import re
+driver.get("https://www.aap.com.au/factcheck/do-eating-disorders-have-the-highest-mortality-rate-of-any-mental-illness/")
+
+# Method 1. Use reviewRating in the script. However, only recent FCs contain this.
+eles = driver.find_elements_by_xpath('//script[@type="application/ld+json"]')
+eles[-1].get_attribute("innerHTML") # 0 gives wrong json
+dic = json.loads(eles[-1].get_attribute("innerHTML"))
+try:
+    dic[0].keys() # only 1 element, but it stored as list.
+except:
+    rating_ex = dic['reviewRating']['alternateName']  # this gives rating & explanation together
+else:
+    rating_ex = dic[0]['reviewRating']['alternateName']  # this gives rating & explanation together
+
+# dic[0].keys()
+rat_li = re.split('–|–|-\.',rating_ex)
+rat_li[0].strip()
+
+# Method 2. Use "–" as a rating indicator
+## not that useful, since COVID-19, Fact-Checking cases.
+
+df.columns
+driver.get("https://www.aap.com.au/factcheck/post-covid-19-injection-syndrome-is-only-garbled-misinformation/")
+ps = driver.find_element_by_class_name('c-article__verdict').find_elements_by_tag_name("p")
+# ps[2].text
+for i in ps:
+    sobj = re.search('[a-zA-Z].{0,100}(–|–|-)',i.text).group()
+    x = re.split("–|–|-", sobj)
+    label = x[0].strip()
+    if len(label) < 20:
+        print(label)
+
+############################################# Updated Rating Crawling ################
+import json
+import re
+df = pd.read_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_090122.csv")
+ratings2 = []
+for i in range(len(df)):
+    driver.get(df['link'][i])
+    # Method 1. Use reviewRating in the script. However, only recent FCs contain this.
+    eles = driver.find_elements_by_xpath('//script[@type="application/ld+json"]')
+    eles[-1].get_attribute("innerHTML")  # 0 gives wrong json
+    dic = json.loads(eles[-1].get_attribute("innerHTML"))
+    try: # when there are multiple dictionaries
+        # dic[0].keys()  # only 1 element, but it stored as list.
+        rating_ex = dic[0]['reviewRating']['alternateName']  # this gives rating & explanation together
+        rat_li = re.split('–|–|-|\.', rating_ex)
+        ratings2.append(rat_li[0].strip())
+    except:
+        try: # when there is only one dictionary
+            rating_ex = dic['reviewRating']['alternateName']  # this gives rating & explanation together
+            rat_li = re.split('–|–|-|\.', rating_ex)
+            ratings2.append(rat_li[0].strip())
+        except: # Method 2
+            ps = driver.find_element_by_class_name('c-article__verdict').find_elements_by_tag_name("p")
+            foo1 = 0
+            # ps[2].text
+            for tt in ps:
+                sobj = re.search('[a-zA-Z].{0,100}(–|–|-)', tt.text)
+                if sobj:
+                    sobj = sobj.group()
+                    x = re.split("–|–|-", sobj)
+                    label = x[0].strip()
+                    if len(label) < 20:
+                        ratings2.append(label)
+                        foo1 += 1
+            if foo1 == 0: # for some cases, rating was created using <li></li>
+                ps = driver.find_element_by_class_name('c-article__verdict').find_elements_by_tag_name("li")
+                foo2 = 0
+                # ps[2].text
+                for tt in ps:
+                    sobj = re.search('[a-zA-Z].{0,100}(–|–|-)', tt.text)
+                    if sobj:
+                        sobj = sobj.group()
+                        x = re.split("–|–|-", sobj)
+                        label = x[0].strip()
+                        if len(label) < 20:
+                            ratings2.append(label)
+                            foo2 += 1
+                if foo2 == 0:
+                    ratings2.append(None)
+    print(str(i) +" "+df['link'][i] +" done")
+
+if len(df) == len(ratings2):
+    print("ok")
+else:
+    print("len diff")
+
+# lowering all char. : e.g., Partly False vs. partly false
+ratings3 = []
+for i in range(len(ratings2)):
+    if ratings2[i]:
+        ratings3.append(ratings2[i].lower())
+    else:
+        ratings3.append(None)
+
+set_rat = list(set(ratings3))
+for i in range(len(set_rat)):
+    print(str(i)+" "+str(set_rat[i]))
+
+ratings2.count(None) == ratings3.count(None) # double checks
+wrong_rat = [1,3,7,8,9,16,19] # index numbers for the wrong ratings from set_rat.
+for i in wrong_rat:
+    print(ratings3.count(set_rat[i]))
+# 0: None => 42 cases. All other indecies only have 1 case.
+
+for i in wrong_rat: # to check the link and rating manually
+    print(df['link'][ratings3.index(set_rat[i])])
+
+right_rat = ['false','mostly false','false','false','false','false','partly false'] # manually collect correct ratings
+
+rr = 0
+for i in wrong_rat:
+    print(ratings3[ratings3.index(set_rat[i])], " ", right_rat[rr])
+    ratings3[ratings3.index(set_rat[i])] = right_rat[rr] # assign correct labels manually
+    rr+=1
+
+df['rating'] = None
+df['rating'] = ratings3
+
 import os
 os.getcwd()
-df = pd.read_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_083022.csv")
-len(df) # Total 841
+
+len(df) # Total 843
 df.columns
 df.isnull().sum()
+df[df['author'].isnull()]
 
 df['fc_date']=pd.to_datetime(df['fc_date'])
 df['fc_year'] = df.apply(lambda row : row['fc_date'].year, axis=1)
@@ -119,8 +309,13 @@ df['fc_day'] = df.apply(lambda row : row['fc_date'].day, axis=1)
 df['fc_year'].isnull().sum()
 df['fc_month'].isnull().sum()
 df['fc_day'].isnull().sum()
+df.to_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_090122.csv",index=False)
 
+######################## Select only 2019 ~ 2021 years' data #######################
 len(df)
 df = df[df['fc_year'].between(2019,2021)] # Select FCs published between 2019 and 2021
 len(df) # total # FCs: 612
 df.to_csv("/Users/agathos/DtotheS/AI-in-the-wild/data/aap_083022_19_21.csv",index=False)
+df.columns
+
+
